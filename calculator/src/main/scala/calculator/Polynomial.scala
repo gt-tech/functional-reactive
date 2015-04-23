ba1237f8c9f08d270d58967d4811ad661aaed326
GT <GT@noreply.com>
2015-04-23 02:09:15 -0400

    minor formatting, status as of previous check-in, EOD 4/22 status

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,6 +3,7 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
                    c: Signal[Double]): Signal[Double] = {
+    // b2 - 4ac
     Signal { math.pow(b(), 2) - (4 * a() * c()) }
   }
 
@@ -16,12 +17,12 @@ object Polynomial {
       // if delta is zero, the set of roots has one member
       // if delta is greater than zero, the set of roots has two members
       delta() match {
-        case 0 => result = result.+ { (b() * -1) / (2 * a()) }
+        case 0 => result = result.+ { (b() * -1) / (2 * a() ) }
         case x if (x > 0) => result = result.+(
           ((b() * -1) + math.sqrt(x)) / (2 * a()),
           ((b() * -1) - math.sqrt(x)) / (2 * a()))
         case _ =>
-      }
+      }          
       result
     }
   }
